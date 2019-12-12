5653709736b5e89d8f4c4e7cc0ffd9237cb28369
supriya <linkin.suppu@gmail.com>
2019-12-12 10:17:05 +0545

    image folder fixed

--- js-final-project/bump-sheep/js/loader.js
+++ js-final-project/bump-sheep/js/loader.js
@@ -1,23 +1,23 @@
 var lane = new Image();
-lane.src = '../images/lane.png';
+lane.src = './images/lane.png';
 
 var bushRight = new Image();
-bushRight.src = '../images/bush.png'
+bushRight.src = './images/bush.png'
 
 var bushLeft = new Image();
-bushLeft.src = '../images/bush2.png'
+bushLeft.src = './images/bush2.png'
 
 var button = new Image();
 button.src = ''
 
 var smallWhiteSprite = new Image();
-smallWhiteSprite.src = '../images/small-white.png';
+smallWhiteSprite.src = './images/small-white.png';
 
 var mediumWhiteSprite = new Image();
-mediumWhiteSprite.src = '../images/medium-white.png'
+mediumWhiteSprite.src = './images/medium-white.png'
 
 var largeWhiteSprite = new Image();
-largeWhiteSprite.src = '../images/large-white.png'
+largeWhiteSprite.src = './images/large-white.png'
 
 var superLargeWhiteSprite = new Image();
-superLargeWhiteSprite.src = '../images/super-large-white.png'
+superLargeWhiteSprite.src = './images/super-large-white.png'
